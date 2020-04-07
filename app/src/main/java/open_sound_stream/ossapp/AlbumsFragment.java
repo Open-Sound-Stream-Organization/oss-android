8ec3842ea7f53af413d4d122bc853b2c16dafa88
Leo <jujutsu.ln@gmail.com>
2020-04-07 04:11:33 +0200

    tabs implemented, first code snippets of bootom sheet

---
+++ app/src/main/java/open_sound_stream/ossapp/AlbumsFragment.java
@@ -0,0 +1,20 @@
+package open_sound_stream.ossapp;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class AlbumsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_albums, container, false);
+    }
+
+
+}
