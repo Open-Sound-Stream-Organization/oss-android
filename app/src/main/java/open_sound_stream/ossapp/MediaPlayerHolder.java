8ee74000556b7fe596bd3ceebd8220a03c92ec99
Konstantin <kon.radke15@gmail.com>
2020-04-07 09:12:03 +0200

    Merged Play and Pause button into one, Prev button restarts the song when over 5 seconds into playback. No background playback yet.

--- app/src/main/java/open_sound_stream/ossapp/MediaPlayerHolder.java
+++ app/src/main/java/open_sound_stream/ossapp/MediaPlayerHolder.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package open_sound_stream.ossapp;
 
 import android.content.Context;
@@ -60,14 +44,12 @@ public final class MediaPlayerHolder implements PlayerAdapter {
                 @Override
                 public void onCompletion(MediaPlayer mediaPlayer) {
                     stopUpdatingCallbackWithPosition(true);
-                    logToUI("MediaPlayer playback completed");
                     if (mPlaybackInfoListener != null) {
                         mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.COMPLETED);
                         mPlaybackInfoListener.onPlaybackCompleted();
                     }
                 }
             });
-            logToUI("mMediaPlayer = new MediaPlayer()");
         }
     }
 
@@ -86,27 +68,21 @@ public final class MediaPlayerHolder implements PlayerAdapter {
         AssetFileDescriptor assetFileDescriptor =
                 mContext.getResources().openRawResourceFd(mResourceId);
         try {
-            logToUI("load() {1. setDataSource}");
             mMediaPlayer.setDataSource(assetFileDescriptor);
         } catch (Exception e) {
-            logToUI(e.toString());
         }
 
         try {
-            logToUI("load() {2. prepare}");
             mMediaPlayer.prepare();
         } catch (Exception e) {
-            logToUI(e.toString());
         }
 
         initializeProgressCallback();
-        logToUI("initializeProgressCallback()");
     }
 
     @Override
     public void release() {
         if (mMediaPlayer != null) {
-            logToUI("release() and mMediaPlayer = null");
             mMediaPlayer.release();
             mMediaPlayer = null;
         }
@@ -123,8 +99,6 @@ public final class MediaPlayerHolder implements PlayerAdapter {
     @Override
     public void play() {
         if (mMediaPlayer != null && !mMediaPlayer.isPlaying()) {
-            logToUI(String.format("playbackStart() %s",
-                                  mContext.getResources().getResourceEntryName(mResourceId)));
             mMediaPlayer.start();
             if (mPlaybackInfoListener != null) {
                 mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.PLAYING);
@@ -137,7 +111,6 @@ public final class MediaPlayerHolder implements PlayerAdapter {
     @Override
     public void reset() {
         if (mMediaPlayer != null) {
-            logToUI("playbackReset()");
             mMediaPlayer.reset();
             loadMedia(mResourceId);
             if (mPlaybackInfoListener != null) {
@@ -154,14 +127,12 @@ public final class MediaPlayerHolder implements PlayerAdapter {
             if (mPlaybackInfoListener != null) {
                 mPlaybackInfoListener.onStateChanged(PlaybackInfoListener.State.PAUSED);
             }
-            logToUI("playbackPause()");
         }
     }
 
     @Override
     public void seekTo(int position) {
         if (mMediaPlayer != null) {
-            logToUI(String.format("seekTo() %d ms", position));
             mMediaPlayer.seekTo(position);
         }
     }
@@ -216,16 +187,10 @@ public final class MediaPlayerHolder implements PlayerAdapter {
         if (mPlaybackInfoListener != null) {
             mPlaybackInfoListener.onDurationChanged(duration);
             mPlaybackInfoListener.onPositionChanged(0);
-            logToUI(String.format("firing setPlaybackDuration(%d sec)",
-                                  TimeUnit.MILLISECONDS.toSeconds(duration)));
-            logToUI("firing setPlaybackPosition(0)");
         }
     }
 
-    private void logToUI(String message) {
-        if (mPlaybackInfoListener != null) {
-            mPlaybackInfoListener.onLogUpdated(message);
-        }
+    public int getCurrentPlaybackPosition() {
+        return mMediaPlayer.getCurrentPosition();
     }
-
 }
