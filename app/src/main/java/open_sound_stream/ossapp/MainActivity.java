a20a702541031eefc0f1f2acb6b9544eeda1ce6f
Konstantin <kon.radke15@gmail.com>
2020-04-07 09:12:03 +0200

    Merged Play and Pause button into one, Prev button restarts the song when over 5 seconds into playback. No background playback yet.

--- app/src/main/java/open_sound_stream/ossapp/MainActivity.java
+++ app/src/main/java/open_sound_stream/ossapp/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package open_sound_stream.ossapp;
 
 import android.os.Build;
@@ -21,12 +5,9 @@ import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 /**
  * Allows playback of a single MP3 file via the UI. It contains a {@link MediaPlayerHolder}
@@ -35,12 +16,9 @@ import android.widget.TextView;
  */
 public final class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
     public static final int MEDIA_RES_ID = R.raw.jazz_in_paris;
 
-    private TextView mTextDebug;
     private SeekBar mSeekbarAudio;
-    private ScrollView mScrollContainer;
     private PlayerAdapter mPlayerAdapter;
     private boolean mUserIsSeeking = false;
 
@@ -51,64 +29,59 @@ public final class MainActivity extends AppCompatActivity {
         initializeUI();
         initializeSeekbar();
         initializePlaybackController();
-        Log.d(TAG, "onCreate: finished");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mPlayerAdapter.loadMedia(MEDIA_RES_ID);
-        Log.d(TAG, "onStart: create MediaPlayer");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (isChangingConfigurations() && mPlayerAdapter.isPlaying()) {
-            Log.d(TAG, "onStop: don't release MediaPlayer as screen is rotating & playing");
-        } else {
+        if (!isChangingConfigurations() && !mPlayerAdapter.isPlaying()) {
             mPlayerAdapter.release();
-            Log.d(TAG, "onStop: release MediaPlayer");
         }
     }
 
     private void initializeUI() {
-        mTextDebug = (TextView) findViewById(R.id.text_debug);
-        Button mPlayButton = (Button) findViewById(R.id.button_play);
-        Button mPauseButton = (Button) findViewById(R.id.button_pause);
-        Button mResetButton = (Button) findViewById(R.id.button_reset);
-        mSeekbarAudio = (SeekBar) findViewById(R.id.seekbar_audio);
-        mScrollContainer = (ScrollView) findViewById(R.id.scroll_container);
+        Button mPlayPauseButton = findViewById(R.id.button_playPause);
+        Button mPrevButton = findViewById(R.id.button_prev);
+        Button mNextButton = findViewById(R.id.button_next);
+        mSeekbarAudio = findViewById(R.id.seekbar_audio);
 
-        mPauseButton.setOnClickListener(
+        mPlayPauseButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        mPlayerAdapter.pause();
+                        if (mPlayerAdapter.isPlaying()) {
+                            mPlayerAdapter.pause();
+                        }
+                        else {
+                            mPlayerAdapter.play();
+                        }
                     }
                 });
-        mPlayButton.setOnClickListener(
+
+        mPrevButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        mPlayerAdapter.play();
-                    }
-                });
-        mResetButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        mPlayerAdapter.reset();
+                        if (mPlayerAdapter.getCurrentPlaybackPosition() <= 5000)
+                        {
+                            //to previous track
+                        } else {
+                            mPlayerAdapter.seekTo(0);
+                        }
                     }
                 });
     }
 
     private void initializePlaybackController() {
         MediaPlayerHolder mMediaPlayerHolder = new MediaPlayerHolder(this);
-        Log.d(TAG, "initializePlaybackController: created MediaPlayerHolder");
         mMediaPlayerHolder.setPlaybackInfoListener(new PlaybackListener());
         mPlayerAdapter = mMediaPlayerHolder;
-        Log.d(TAG, "initializePlaybackController: MediaPlayerHolder progress callback set");
     }
 
     private void initializeSeekbar() {
@@ -141,7 +114,6 @@ public final class MainActivity extends AppCompatActivity {
         @Override
         public void onDurationChanged(int duration) {
             mSeekbarAudio.setMax(duration);
-            Log.d(TAG, String.format("setPlaybackDuration: setMax(%d)", duration));
         }
 
         @RequiresApi(api = Build.VERSION_CODES.N)
@@ -149,34 +121,15 @@ public final class MainActivity extends AppCompatActivity {
         public void onPositionChanged(int position) {
             if (!mUserIsSeeking) {
                 mSeekbarAudio.setProgress(position, true);
-                Log.d(TAG, String.format("setPlaybackPosition: setProgress(%d)", position));
             }
         }
 
         @Override
         public void onStateChanged(@State int state) {
-            String stateToString = PlaybackInfoListener.convertStateToString(state);
-            onLogUpdated(String.format("onStateChanged(%s)", stateToString));
         }
 
         @Override
         public void onPlaybackCompleted() {
-        }
-
-        @Override
-        public void onLogUpdated(String message) {
-            if (mTextDebug != null) {
-                mTextDebug.append(message);
-                mTextDebug.append("\n");
-                // Moves the scrollContainer focus to the end.
-                mScrollContainer.post(
-                        new Runnable() {
-                            @Override
-                            public void run() {
-                                mScrollContainer.fullScroll(ScrollView.FOCUS_DOWN);
-                            }
-                        });
-            }
         }
     }
 }
