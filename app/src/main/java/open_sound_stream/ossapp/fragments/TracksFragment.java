5c5f743518b1f630fc0e9294ced814e1014ce8af
Leo <jujutsu.ln@gmail.com>
2020-04-27 01:05:16 +0200

    added context menu

--- app/src/main/java/open_sound_stream/ossapp/fragments/TracksFragment.java
+++ app/src/main/java/open_sound_stream/ossapp/fragments/TracksFragment.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -28,6 +30,7 @@ import open_sound_stream.ossapp.db.entities.Track;
 public class TracksFragment extends Fragment {
 
     private View RootView = null;
+    private Track selectedTrack = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -45,6 +48,52 @@ public class TracksFragment extends Fragment {
 
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.listview) {
+
+
+            //copied from StOv
+            ListView lv = (ListView) v;
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            Track obj = (Track) lv.getItemAtPosition(acmi.position);
+
+            this.selectedTrack = obj;
+
+            menu.setHeaderTitle(obj.getTitle());
+            menu.add("Download");
+            menu.add("Next Track");
+            menu.add("Add to Queue");
+
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getTitle().toString()) {
+            case "Download":
+
+                Toast.makeText(this.getContext(), this.selectedTrack.getTitle() + " wird gedwonloadet", Toast.LENGTH_LONG).show();
+                this.selectedTrack = null;
+                return true;
+
+            case "Next Track":
+
+                Toast.makeText(this.getContext(), this.selectedTrack.getTitle() + " wird als nächstes gespielt", Toast.LENGTH_LONG).show();
+                this.selectedTrack = null;
+                return true;
+
+            case "Add to Queue":
+
+                Toast.makeText(this.getContext(), this.selectedTrack.getTitle() + " wird in die wdgliste eingefügt", Toast.LENGTH_LONG).show();
+                this.selectedTrack = null;
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
     public void UpdateTracksFragment(Context context){
 
         OSSRepository db = new OSSRepository(context);
@@ -58,6 +107,7 @@ public class TracksFragment extends Fragment {
                 ArrayAdapter<Track> arrayAdapter = new ArrayAdapter<Track>(getActivity().getApplicationContext(), R.layout.list_item, allTracksArray );
                 listview.setAdapter(arrayAdapter);
 
+                registerForContextMenu(listview);
 
 
                 Log.d("updateDB", "trackfragment geupdatet");
